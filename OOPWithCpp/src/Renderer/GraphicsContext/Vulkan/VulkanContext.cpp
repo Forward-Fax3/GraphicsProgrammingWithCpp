bcfd5491334929c61b962552797c0af1d0a7025a
Forward-Fax3 <dfordommmmm@gmail.com>
2025-11-25 23:01:23 +0000

    improved vulkan validation dubug callback

--- OOPWithCpp/src/Renderer/GraphicsContext/Vulkan/VulkanContext.cpp
+++ OOPWithCpp/src/Renderer/GraphicsContext/Vulkan/VulkanContext.cpp
@@ -1,4 +1,5 @@
 #include <limits>
+#include <mutex>
 
 #include <vulkan/vulkan.hpp>
 #include <vulkan/vulkan_to_string.hpp>
@@ -12,7 +13,7 @@
 PFN_vkCreateDebugUtilsMessengerEXT pfnVkCreateDebugUtilsMessengerEXT;
 PFN_vkDestroyDebugUtilsMessengerEXT pfnVkDestroyDebugUtilsMessengerEXT;
 
-VKAPI_ATTR VkResult VKAPI_CALL vkCreateDebugUtilsMessengerEXT(
+static VKAPI_ATTR VkResult VKAPI_CALL vkCreateDebugUtilsMessengerEXT(
 	VkInstance instance,
 	const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
 	const VkAllocationCallbacks* pAllocator,
@@ -21,12 +22,116 @@ VKAPI_ATTR VkResult VKAPI_CALL vkCreateDebugUtilsMessengerEXT(
 	return pfnVkCreateDebugUtilsMessengerEXT(instance, pCreateInfo, pAllocator, pMessenger);
 }
 
-VKAPI_ATTR void VKAPI_CALL vkDestroyDebugUtilsMessengerEXT(
+static VKAPI_ATTR void VKAPI_CALL vkDestroyDebugUtilsMessengerEXT(
 	VkInstance instance,
 	VkDebugUtilsMessengerEXT messenger,
 	VkAllocationCallbacks const* pAllocator)
 {
 	return pfnVkDestroyDebugUtilsMessengerEXT(instance, messenger, pAllocator);
+}
+
+static void PrintVulkanDebugMessages(vk::DebugUtilsMessageSeverityFlagBitsEXT messageSeverity, const std::vector<std::string>& s_MessagesLogged)
+{
+	using enum vk::DebugUtilsMessageSeverityFlagBitsEXT;
+	using enum OWC::LogLevel;
+
+	std::string loggedMessage;
+	for (const auto& msg : s_MessagesLogged)
+		loggedMessage += "\t " + msg + "\n";
+
+	if (messageSeverity & eError)
+		OWC::Log<Error>("Vulkan Validation Layer:\n{}", loggedMessage);
+	else if (messageSeverity & eWarning)
+		OWC::Log<Warn>("Vulkan Validation Layer:\n{}", loggedMessage);
+	else if (messageSeverity & eInfo)
+		OWC::Log<Trace>("Vulkan Validation Layer:\n{}", loggedMessage);
+	else
+		OWC::Log<Trace>("Vulkan Validation Layer:\n{}", loggedMessage);
+}
+
+static VKAPI_ATTR vk::Bool32 VKAPI_CALL debugMessageFunc( // TODO: add objects info logging
+	vk::DebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+	vk::DebugUtilsMessageTypeFlagsEXT messageTypes,
+	vk::DebugUtilsMessengerCallbackDataEXT const* pCallbackData,
+	void* /*pUserData*/)
+{
+	static std::mutex s_Mutex;
+	static std::vector<std::string> s_MessagesLogged;
+	static int32_t lastMessageID = std::numeric_limits<int32_t>::max();
+	static vk::DebugUtilsMessageSeverityFlagBitsEXT lastMessageSeverity = vk::DebugUtilsMessageSeverityFlagBitsEXT();
+	static bool firstMessage = true;
+
+	if (pCallbackData == nullptr)
+	{
+		std::lock_guard lock(s_Mutex);
+		PrintVulkanDebugMessages(lastMessageSeverity, s_MessagesLogged);
+		s_MessagesLogged.clear();
+		lastMessageID = std::numeric_limits<int32_t>::max();
+		lastMessageSeverity = vk::DebugUtilsMessageSeverityFlagBitsEXT();
+		firstMessage = true;
+		return VK_FALSE;
+	}
+
+	std::string str = std::format(
+		"{} {}: {}",
+		pCallbackData->pMessageIdName,
+		vk::to_string(messageTypes),
+		pCallbackData->pMessage ? pCallbackData->pMessage : "NULL"
+	);
+
+	if (pCallbackData->queueLabelCount > 0)
+	{
+		str += "\n\t Queue Labels:";
+		for (uint32_t i = 0; i < pCallbackData->queueLabelCount; i++)
+		{
+			str += std::format("\n\t\t labelName = <{}>",
+				pCallbackData->pQueueLabels[i].pLabelName ? pCallbackData->pQueueLabels[i].pLabelName : "NULL");
+		}
+	}
+	if (pCallbackData->cmdBufLabelCount > 0)
+	{
+		str += "\n\t CommandBuffer Labels:";
+		for (uint32_t i = 0; i < pCallbackData->cmdBufLabelCount; i++)
+		{
+			str += std::format("\n\t\t labelName = <{}>",
+				pCallbackData->pCmdBufLabels[i].pLabelName ? pCallbackData->pCmdBufLabels[i].pLabelName : "NULL");
+		}
+	}
+//	if (pCallbackData->objectCount > 0 /* && !std::string("NULL").compare(pCallbackData->pObjects[0].pObjectName) */)
+//	{
+//		str += "\n\t Objects:";
+//		for (uint32_t i = 0; i < pCallbackData->objectCount; i++)
+//		{
+//			str += std::format(
+//				"\n\t\t objectName = <{}>"
+//				"\n\t\t objectType = {}",
+//				pCallbackData->pObjects[i].pObjectName ? pCallbackData->pObjects[i].pObjectName : "NULL",
+//				vk::to_string(pCallbackData->pObjects[i].objectType)
+//			);
+//		}
+//	}
+
+	// leave the lock as late as possible
+	std::lock_guard lock(s_Mutex);
+	if (pCallbackData->messageIdNumber == lastMessageID && messageSeverity == lastMessageSeverity)
+		s_MessagesLogged.emplace_back(std::move(str));
+	else if (firstMessage)
+	{
+		s_MessagesLogged.emplace_back(std::move(str));
+		lastMessageID = pCallbackData->messageIdNumber;
+		lastMessageSeverity = messageSeverity;
+		firstMessage = false;
+	}
+	else
+	{
+		PrintVulkanDebugMessages(lastMessageSeverity, s_MessagesLogged);
+		s_MessagesLogged.clear();
+		s_MessagesLogged.emplace_back(std::move(str));
+		lastMessageID = pCallbackData->messageIdNumber;
+		lastMessageSeverity = messageSeverity;
+	}
+
+	return VK_FALSE;
 }
 
 namespace OWC::Graphics
@@ -55,10 +160,19 @@ namespace OWC::Graphics
 		{
 			Log<LogLevel::Critical>("Vulkan Context initialization failed: {}", ex.what());
 		}
+
+		// flush any logged messages during initialization
+		debugMessageFunc(
+			vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo,
+			vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral,
+			nullptr,
+			nullptr
+		);
 	}
 
 	VulkanContext::~VulkanContext()
 	{
+		WaitForIdle();
 		auto& vkCore = VulkanCore::GetConstInstance();
 
 		SDL_Vulkan_DestroySurface(vkCore.GetVKInstance(), vkCore.GetSurface(), nullptr);
@@ -144,25 +258,7 @@ namespace OWC::Graphics
 				vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral |
 				vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance |
 				vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation
-			).setPfnUserCallback( // TODO: do more with pCallbackData
-				[](
-					vk::DebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-					vk::DebugUtilsMessageTypeFlagsEXT messageTypes,
-					const vk::DebugUtilsMessengerCallbackDataEXT* pCallbackData,
-					void* // unused userData
-					) -> vk::Bool32
-				{
-					if (messageSeverity & vk::DebugUtilsMessageSeverityFlagBitsEXT::eError)
-						Log<LogLevel::Error>("Vulkan Validation Layer: {}: {}", vk::to_string(messageTypes), pCallbackData->pMessage);
-					else if (messageSeverity & vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning)
-						Log<LogLevel::Warn>("Vulkan Validation Layer: {}: {}", vk::to_string(messageTypes), pCallbackData->pMessage);
-					else if (messageSeverity & vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo)
-						Log<LogLevel::Trace>("Vulkan Validation Layer: {}: {}", vk::to_string(messageTypes), pCallbackData->pMessage);
-					else
-						Log<LogLevel::Trace>("Vulkan Validation Layer: {}: {}", vk::to_string(messageTypes), pCallbackData->pMessage);
-					return VK_FALSE;
-				}
-			)
+			).setPfnUserCallback(debugMessageFunc)
 		);
 	}
 #endif
