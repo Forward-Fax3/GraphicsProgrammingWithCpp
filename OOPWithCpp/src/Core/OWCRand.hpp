4e9941ac889684526e480e4b2afa485b708385fa
Forward-Fax3 <dfordommmmm@gmail.com>
2026-01-19 13:03:20 +0000

    change rnadom function to use c++ random

--- OOPWithCpp/src/Core/OWCRand.hpp
+++ OOPWithCpp/src/Core/OWCRand.hpp
@@ -1,62 +1,34 @@
 ﻿#pragma once
 #include "Core.hpp"
 
+#include <random>
 #include <array>
 
 
 namespace OWC::Rand
 {
+	static thread_local std::minstd_rand globalMtEngine;
+
 	OWC_FORCE_INLINE Vec2 LinearFastRandVec2(const Vec2& min, const Vec2& max)
 	{
-		static thread_local std::array<u64, 4> stats = { 123456789u, 362436069u, 521288629u, 88675123u };
-
-		uint64_t temp = stats[3];
-		uint64_t s = stats[0];
-		stats[3] = stats[2];
-		stats[2] = stats[1];
-		stats[1] = s;
-
-		// uses masks to maintain 32-bit values in each part of the uint64_t
-		temp ^= (temp << 11) & 0b1111111111111111111110000000000011111111111111111111100000000000;
-		temp ^= (temp >> 8) & 0b0000000011111111111111111111111100000000111111111111111111111111;
-		stats[0] = temp ^ s ^ ((s >> 19) & 0b0000000000000000000111111111111100000000000000000001111111111111);
-//		temp ^= (temp << 11);
-//		temp ^= (temp >> 8);
-//		stats[0] = temp ^ s ^ (s >> 19);
+		static thread_local std::uniform_real_distribution<f32> dist;
 		Vec2 randFloats(
-			static_cast<f32>(stats[0] & 0xFFFFFFFF) * (1.0f / static_cast<f32>(std::numeric_limits<u32>::max())),
-			static_cast<f32>((stats[0] >> 32) & 0xFFFFFFFF) * (1.0f / static_cast<f32>(std::numeric_limits<u32>::max()))
+			dist(globalMtEngine),
+			dist(globalMtEngine)
 		);
 		return min + (max - min) * randFloats;
 	}
 
 	OWC_FORCE_INLINE Vec4 LinearFastRandVec4(const Vec4& min, const Vec4& max)
 	{
-		// TODO: fix MSVC shenanigans being super slow
+		static thread_local std::uniform_real_distribution<f32> dist;
 
-		static thread_local std::array<Vec4u32, 4> states = {
-			Vec4u32{ 123456789u, 362436069u, 521288629u, 88675123u },
-			Vec4u32{ 5783321u, 6615241u, 1234567u, 89101112u },
-			Vec4u32{ 42424242u, 42424243u, 42424244u, 42424245u },
-			Vec4u32{ 987654321u, 123456789u, 192837465u, 564738291u }
-		};
-
-		Vec4u32 temp = states[3];
-
-		Vec4u32 s = states[0];
-		states[3] = states[2];
-		states[2] = states[1];
-		states[1] = s;
-
-		Vec4u32 tempShifted{};
-		tempShifted.data = _mm_sll_epi32(temp.data, _mm_set_epi64x(0, 11));
-		temp.data = _mm_xor_si128(temp.data, tempShifted.data);
-		tempShifted.data = _mm_srl_epi32(temp.data, _mm_set_epi64x(0, 8));
-		temp.data = _mm_xor_si128(temp.data, tempShifted.data);
-
-		tempShifted.data = _mm_srl_epi32(s.data, _mm_set_epi64x(0, 19));
-		states[0] = temp ^ s ^ tempShifted;
-		Vec4 randFloats = Vec4(states[0]) * (1.0f / static_cast<f32>(std::numeric_limits<u32>::max()));
+		Vec4 randFloats(
+			dist(globalMtEngine),
+			dist(globalMtEngine),
+			dist(globalMtEngine),
+			dist(globalMtEngine)
+		);
 		return min + (max - min) * randFloats;
 	}
 
@@ -65,12 +37,12 @@ namespace OWC::Rand
 		return LinearFastRandVec4(Vec4(min, 0.0f), Vec4(max, 0.0f)); // implicit conversion
 	}
 
-	OWC_FORCE_INLINE Vec3 FastUnitVecctor()
+	OWC_FORCE_INLINE Vec3 FastUnitVector()
 	{
 		return glm::normalize(LinearFastRandVec3(Vec3(-1.0), Vec3(1.0)));
 	}
 
-	OWC_FORCE_INLINE Vec4 FastUnitVecctor4()
+	OWC_FORCE_INLINE Vec4 FastUnitVector4()
 	{
 		return glm::normalize(LinearFastRandVec4(Vec4(-1.0), Vec4(1.0)));
 	}
